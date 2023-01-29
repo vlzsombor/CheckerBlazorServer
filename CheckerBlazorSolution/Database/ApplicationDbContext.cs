59a388940cfc6de5ddb086f2462ced3270db7d77
Zsombor Veres-Lakos <vl.zsombor@hotmail.com>
2023-01-29 20:31:47 +0100

    test

--- CheckerBlazorSolution/Database/ApplicationDbContext.cs
+++ CheckerBlazorSolution/Database/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using CheckerBlazorServer.Database.Dtos;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CheckerBlazorServer.Database;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : IdentityDbContext
 {
     public DbSet<TestDto> Test { get; set; }
 
@@ -12,4 +13,3 @@ public class ApplicationDbContext : DbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
 }
-
